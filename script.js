dbe336c070f5a3db1ba6645a946a3d90b63992f6
RoriCrimson <haleemam421@gmail.com>
2022-10-18 16:57:11 +0600

    bug fixed

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const boxWrapper = document.querySelectorAll(".box-wrapper");
 const box = document.querySelectorAll(".box");
+const money = document.querySelectorAll(".money");
 
-fetch(`${window.location.origin + window.location.pathname}data.json`)
+fetch(
+  // "http://127.0.0.1:5500/data.json"
+  `${
+    window.location.origin + window.location.pathname.replace(/index.html/g, "")
+  }data.json`
+)
   .then((res) => res.json())
   .then((res) => {
     let max = 0;
@@ -11,10 +17,22 @@ fetch(`${window.location.origin + window.location.pathname}data.json`)
       }
     });
     res.forEach((el, index) => {
+      var maxBox;
       if (el.amount === max) {
+        maxBox = box[index];
         box[index].style.backgroundColor = "hsl(186, 34%, 60%)";
       }
       let height = (el.amount / max) * 100;
       boxWrapper[index].style.height = height + "%";
+      box[index].onmouseover = () => {
+        money[index].innerHTML = "$" + el.amount;
+        money[index].style.display = "block";
+        maxBox.style.backgroundColor = "hsl(187, 47%, 78%)";
+      };
+      boxWrapper[index].style.height = height + "%";
+      box[index].onmouseleave = () => {
+        money[index].style.display = "none";
+        maxBox.style.backgroundColor = "hsl(186, 34%, 60%)";
+      };
     });
   });
